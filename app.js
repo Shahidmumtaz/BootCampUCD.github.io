e3ed40b643fc675b64978520f003b78c6845038d
BootCampUCD <56660232+BootCampUCD@users.noreply.github.com>
2020-04-21 19:54:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let sampleData = d3.json("samples.json");
+console.log(sampleData);
+
+// function readTextFile("./samples.json", function (text) {
+//   var data = JSON.parse(text);
+//   console.log(data);
+// });
+
+// readTextFile()
+
+// const sampleData = samples;
+
+// console.log(sampleData);
+
+// d3.selectAll("#selDataset").on("change", optionChanged);
+
+// function optionChanged() {
+//   var dropdownMenu = d3.selectAll("#selDataset");
+//   var dataset = dropdownMenu.property("value");
+
+//   var dropdownMenuID = sampleData.names;
+//   var selectedOption = sampleData.value;
+//   console.log(dropdownMenu);
+//   console.log(selectedOption);
+// }
