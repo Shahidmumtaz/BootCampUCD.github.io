909ff6bb149b6340b052903998f4bf2f21332980
BootCampUCD <56660232+BootCampUCD@users.noreply.github.com>
2020-05-13 14:38:07 -0700

    Add files via upload

---
+++ config-P3.js
@@ -0,0 +1,16 @@
+// // API key for Bureau Labor Statistics
+// const API_KEY_BLS = "baf0e2a656b742848d3f9fb931d7eb7d";
+
+// // API key for Quandl
+// const API_KEY_Quandl = "YUfptP1SiqAycmNpc2mW";
+
+// const API_KEY_ = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+// # OpenWeatherMap API Key
+// const API_KEY_Weather = "c65b9cd00fdf67b40fb41ab0238fc98e"
+
+// # Google API Key
+// const API_KEY_Google = "AIzaSyBEDywOKM7I9fnWYiId6kf0_RBmFFx69GI"
+
+// # Mapbox API Key
+const API_KEY =
+  "pk.eyJ1IjoibWFpbDJ2dSIsImEiOiJjazlmdmpjNnQwaGd5M2ZxdG9xNnJ4NTdjIn0.6AzKSUE0Ajqz-MANbyFllg";
